da0717bf798478bb3d2fa183a3754e8b27a885a2
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2025-11-08 15:15:05 +0700

    Migration tailwind relatted from gatsby

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,15 @@ import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    language: z.string(),
-    title: z.string(),
-    date: z.date(),
-    categories: z.array(z.string()).optional(), // Changed from reference to string array
-    // slug: z.string(),
-    coverImage: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      language: z.string(),
+      title: z.string(),
+      date: z.date(),
+      categories: z.array(z.string()).optional(), // Changed from reference to string array
+      // slug: z.string(),
+      coverImage: image().optional(),
+    }),
 });
 
 const categoryCollection = defineCollection({
@@ -17,7 +18,21 @@ const categoryCollection = defineCollection({
   schema: z.object({ title: z.string(), description: z.string() }),
 });
 
+const tailwindComponentsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      language: z.string(),
+      title: z.string(),
+      date: z.date(),
+      categories: z.array(z.string()).optional(),
+      slug: z.string().optional(),
+      coverImage: image().optional(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
   categories: categoryCollection,
+  "tailwind-components": tailwindComponentsCollection,
 };
